c8511d9940d4917592be9b0f925572cd960ff043
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-04-21 07:04:37 -0300

    render components in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
+      <Skills />
+      <Contact />
     </div>
   );
 }

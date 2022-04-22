21f534766b76086eb554f55c709019e11f314ee8
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-04-22 00:40:55 -0300

    Contact and forms created, ready for deploy

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,8 +2,46 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div
+      name="contact"
+      className="w-full h-full bg-[#0a192f] flex justify-center pt-20 items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/1ecb08ca-0ead-4f94-9de5-7531e98ebad2"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#64ffda] text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            // Enviame el formulario o contactame por Email -
+            orozcorodri8@gmail.com
+          </p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-[#64ffda] hover:border-[#64ffda] hover:text-[#0a192f] hover:font-bold px-4 py-3 my-8 mx-auto flex items-center">
+          Conversemos!
+        </button>
+      </form>
     </div>
   );
 };
